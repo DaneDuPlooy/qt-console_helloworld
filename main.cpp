3314b3d9e333f4bd8928eb6d1bbc32b4fb410a54
Dane du Plooy <dane@dizzy.co.za>
2024-01-01 17:29:01 +0200

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <QCoreApplication>
+
+int main(int argc, char *argv[])
+{
+    //QCoreApplication a(argc, argv);
+
+    qInfo("Hello");
+    qInfo("there");
+    qInfo("world!");
+
+    //return a.exec();
+    return 0;
+}
